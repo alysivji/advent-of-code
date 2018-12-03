3b9a5850755863f8412a55aa0117f76cc9f797db
Aly Sivji <alysivji@gmail.com>
2018-12-03 08:40:17 -0600

    Complete day 3

--- 2018/README.md
+++ 2018/README.md
@@ -2,13 +2,15 @@
 
 [AoC 2018](https://adventofcode.com/2018)
 
-## [Day 1](https://adventofcode.com/2018/day/1)
+## [Day 1: Chronal Calibration](https://adventofcode.com/2018/day/1)
 
 This one was fairly straight forward. Right now it's about getting back into the flow. Start developing some tooling to make easier to perform repetitive actions.
 
-## [Day 2](https://adventofcode.com/2018/day/2)
+## [Day 2: Inventory Management System](https://adventofcode.com/2018/day/2)
 
 My first pass thru this was messy as I used generators, but I cleaned it up by adding another for loop with a break. This improved runtime:
+
+TIL: Change `continue` to `break`
 
 ```console
 In [9]: %timeit find_prototype_box_common_letters(boxes)
@@ -17,3 +19,23 @@ In [9]: %timeit find_prototype_box_common_letters(boxes)
 In [10]: %timeit find_prototype_box_single_pass(boxes)
 19.9 ms ± 191 µs per loop (mean ± std. dev. of 7 runs, 10 loops each)
 ```
+
+## [Day 3: No Matter How You Slice It](https://adventofcode.com/2018/day/3)
+
+Got it done before starting work. As always, had to refer to the [Regular Expression HOWTO](https://docs.python.org/3/howto/regex.html) and [regexr](https://regexr.com) to hammer something out.
+
+TIL how to pull numbers with a quick regex ([h/t thread on /r/adventofcode](https://www.reddit.com/r/adventofcode/comments/a2lesz/2018_day_3_solutions/eazev7m/))
+
+Since everything we are processing is a number and in the same order, we can use `re.findall` to pull all numbers and create a Claim object.
+
+```python
+def load_input_improved(lines: List[str]) -> List[Claim]:
+    claims = [Claim(*map(int, re.findall(r"-?\d+", line))) for line in lines]
+    return claims
+```
+
+## [Day 4: ](https://adventofcode.com/2018/day/4)
+
+[text]
+
+TIL [text]
---
+++ 2018/day3_cuts.py
@@ -0,0 +1,87 @@
+from collections import defaultdict
+import re
+from typing import Dict, List, NamedTuple, Tuple
+
+CUT = (
+    r"#(?P<num>\d+) @ (?P<left>\d+),(?P<top>\d+): (?P<width>\d+)+x(?P<height>\d+)"
+)  # noqa
+
+
+class Claim(NamedTuple):
+    num: int
+    left: int
+    top: int
+    width: int
+    height: int
+
+
+def load_input(lines: List[str]) -> List[Claim]:
+    p = re.compile(CUT)
+    claims = []
+    for line in lines:
+        cleaned_line = line.strip()
+        m = p.match(cleaned_line)
+        claim = Claim(
+            num=int(m.group("num")),
+            left=int(m.group("left")),
+            top=int(m.group("top")),
+            width=int(m.group("width")),
+            height=int(m.group("height")),
+        )
+        claims.append(claim)
+    return claims
+
+
+def load_input_improved(lines: List[str]) -> List[Claim]:
+    claims = [Claim(*map(int, re.findall(r"-?\d+", line))) for line in lines]
+    return claims
+
+
+def process_cloth(claims: List[Claim]) -> Dict[Tuple[int, int], List[int]]:
+    cloth: Dict[Tuple[int, int], List[int]] = defaultdict(list)
+    for claim in claims:
+        for x in range(claim.left + 1, claim.left + claim.width + 1):
+            for y in range(claim.top + 1, claim.top + claim.height + 1):
+                cloth[(x, y)].append(claim.num)
+    return cloth
+
+
+def find_overlap(claims: List[Claim]) -> int:
+    cloth = process_cloth(claims)
+    overlap = 0
+    for key, value in cloth.items():
+        if len(value) > 1:
+            overlap += 1
+    return overlap
+
+
+def nonoverlapping_claim(claims: List[Claim]) -> int:
+    all_claims = {claim.num: claim for claim in claims}
+    cloth = process_cloth(claims)
+
+    for key, value in cloth.items():
+        if len(value) > 1:
+            for claim in value:
+                all_claims.pop(claim, None)
+
+    if len(all_claims) > 1:
+        raise ValueError
+    return list(all_claims.keys())[0]
+
+
+test_input = """#1 @ 1,3: 4x4
+#2 @ 3,1: 4x4
+#3 @ 5,5: 2x2""".split(
+    "\n"
+)
+test_claims = load_input_improved(test_input)
+assert find_overlap(test_claims) == 4
+assert nonoverlapping_claim(test_claims) == 3
+
+
+if __name__ == "__main__":
+    with open("day3_input.txt", "r") as f:
+        lines = f.readlines()
+    claims = load_input_improved(lines)
+    print(find_overlap(claims))
+    print(nonoverlapping_claim(claims))
