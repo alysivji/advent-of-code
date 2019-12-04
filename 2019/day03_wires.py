80ef168662bf550d3584f18df1919c3ff9a63778
Aly Sivji <alysivji@gmail.com>
2019-12-03 22:31:46 -0600

    complete day 3 -- part 2

--- 2019/day03_wires.py
+++ 2019/day03_wires.py
@@ -1,5 +1,5 @@
 from collections import defaultdict
-from typing import List, NamedTuple, Set
+from typing import Dict, List, NamedTuple, Set
 
 import pytest
 
@@ -19,9 +19,10 @@ def convert_movement_to_list(path: str) -> List[Segment]:
     return [Segment(segment[0], int(segment[1:])) for segment in segments]
 
 
-def trace_wire_on_grid(segments: List[Segment]) -> Set[Coordinate]:
-    wire_path = set()
+def trace_wire_on_grid(segments: List[Segment]) -> Dict[Coordinate, int]:
+    wire_path = {}
     current_coordinate = Coordinate(0, 0)  # start at (0, 0)
+    step = 1
 
     for segment in segments:
         for idx in range(1, segment.distance + 1):
@@ -36,33 +37,77 @@ def trace_wire_on_grid(segments: List[Segment]) -> Set[Coordinate]:
             new_x = current_coordinate.x + direction.x
             new_y = current_coordinate.y + direction.y
             current_coordinate = Coordinate(new_x, new_y)
-            wire_path.add(current_coordinate)
+            wire_path[current_coordinate] = step
+            step += 1
 
     return wire_path
 
 
-def find_manhattan_distance_to_closest_interesection(wire1_locations, wire2_locations):
+def find_manhattan_distance_to_closest_interesection(
+    wire1_locations: Set[Coordinate], wire2_locations: Set[Coordinate]
+) -> int:
     intersections = wire1_locations.intersection(wire2_locations)
     return min(abs(x) + abs(y) for x, y in intersections)
 
 
-def find_closest_distance_given_wire_path(wire1_path, wire2_path):
+def find_closest_distance_given_wire_path(wire1_path: str, wire2_path: str) -> int:
     wire1_segements = convert_movement_to_list(wire1_path)
     wire1_locations = trace_wire_on_grid(wire1_segements)
 
     wire2_segments = convert_movement_to_list(wire2_path)
     wire2_locations = trace_wire_on_grid(wire2_segments)
 
-    return find_manhattan_distance_to_closest_interesection(wire1_locations, wire2_locations)
+    return find_manhattan_distance_to_closest_interesection(
+        set(wire1_locations.keys()), set(wire2_locations.keys())
+    )
 
 
-@pytest.mark.parametrize("wire1_path, wire2_path, expected_distance", (
-    ("R8,U5,L5,D3", "U7,R6,D4,L4", 6),
-    ("R75,D30,R83,U83,L12,D49,R71,U7,L72", "U62,R66,U55,R34,D71,R55,D58,R83", 159),
-    ("R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51", "U98,R91,D20,R16,D67,R40,U7,R15,U6,R7", 135),
-))
-def test_find_closest_distance_given_wire_path(wire1_path, wire2_path, expected_distance):
+@pytest.mark.parametrize(
+    "wire1_path, wire2_path, expected_distance",
+    (
+        ("R8,U5,L5,D3", "U7,R6,D4,L4", 6),
+        ("R75,D30,R83,U83,L12,D49,R71,U7,L72", "U62,R66,U55,R34,D71,R55,D58,R83", 159),
+        (
+            "R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51",
+            "U98,R91,D20,R16,D67,R40,U7,R15,U6,R7",
+            135,
+        ),
+    ),
+)
+def test_find_closest_distance_given_wire_path(
+    wire1_path, wire2_path, expected_distance
+):
     distance = find_closest_distance_given_wire_path(wire1_path, wire2_path)
+    assert distance == expected_distance
+
+
+def minimize_signal_delay_given_wire_path(wire1_path: str, wire2_path: str) -> int:
+    wire1_segements = convert_movement_to_list(wire1_path)
+    wire1_locations = trace_wire_on_grid(wire1_segements)
+
+    wire2_segments = convert_movement_to_list(wire2_path)
+    wire2_locations = trace_wire_on_grid(wire2_segments)
+
+    intersection = set(wire1_locations.keys()).intersection(set(wire2_locations.keys()))
+    return min(wire1_locations[point] + wire2_locations[point] for point in intersection)
+
+
+@pytest.mark.parametrize(
+    "wire1_path, wire2_path, expected_distance",
+    (
+        ("R8,U5,L5,D3", "U7,R6,D4,L4", 30),
+        ("R75,D30,R83,U83,L12,D49,R71,U7,L72", "U62,R66,U55,R34,D71,R55,D58,R83", 610),
+        (
+            "R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51",
+            "U98,R91,D20,R16,D67,R40,U7,R15,U6,R7",
+            410,
+        ),
+    ),
+)
+def test_minimize_signal_delay_given_wire_path(
+    wire1_path, wire2_path, expected_distance
+):
+    distance = minimize_signal_delay_given_wire_path(wire1_path, wire2_path)
     assert distance == expected_distance
 
 
@@ -72,4 +117,7 @@ if __name__ == "__main__":
         wire2_path = f.readline()
 
     distance = find_closest_distance_given_wire_path(wire1_path, wire2_path)
-    print(f"Closest distance to interesction is: {distance}")
+    print(f"Closest distance to intersection is: {distance}")
+
+    min_steps = minimize_signal_delay_given_wire_path(wire1_path, wire2_path)
+    print(f"Min steps to intersection is: {min_steps}")
