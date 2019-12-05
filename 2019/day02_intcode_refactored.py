005d1757dd91f83a2e15bfd8222ad08409197400
Aly Sivji <alysivji@gmail.com>
2019-12-05 08:35:18 -0600

    move IntCodeComputer to separate module

--- 2019/day02_intcode_refactored.py
+++ 2019/day02_intcode_refactored.py
@@ -1,78 +1,11 @@
 from typing import List, NamedTuple
-
 import pytest
-
-
-class Halt(Exception):
-    pass
+from utils import IntCodeComputer
 
 
 class TunedParameters(NamedTuple):
     noun: int
     verb: int
-
-
-class IntCodeComputer:
-    def __init__(self, program: List[int]):
-        self.program = program
-        self.instruction_pointer = 0
-        self.possible_operations = [Add, Multiple, Terminate]
-
-    def process(self):
-        while True:
-            op_code = self.program[self.instruction_pointer]
-            for op in self.possible_operations:
-                if op.match(op_code):
-                    break
-            else:
-                raise ValueError("Invalid op code")
-
-            operation = op(self.program, self.instruction_pointer)
-            try:
-                operation.execute()
-            except Halt:
-                break
-            self.instruction_pointer += operation.num_parameters + 1
-
-        return self.program
-
-
-class Operation:
-    def __init__(self, program, instruction_pointer):
-        self.program = program
-        self.instruction_pointer = instruction_pointer
-
-    @classmethod
-    def match(cls, code):
-        return code == cls.OP_CODE
-
-
-class Add(Operation):
-    OP_CODE = 1
-    num_parameters = 3
-
-    def execute(self):
-        code = self.program
-        idx = self.instruction_pointer
-        code[code[idx + 3]] = code[code[idx + 1]] + code[code[idx + 2]]
-
-
-class Multiple(Operation):
-    OP_CODE = 2
-    num_parameters = 3
-
-    def execute(self):
-        code = self.program
-        idx = self.instruction_pointer
-        code[code[idx + 3]] = code[code[idx + 1]] * code[code[idx + 2]]
-
-
-class Terminate(Operation):
-    OP_CODE = 99
-    num_parameters = 0
-
-    def execute(self):
-        raise Halt
 
 
 @pytest.mark.parametrize(
