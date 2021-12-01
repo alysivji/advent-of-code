58d6cbc24d79d01a3d8ebd1ae8ce1718e665ee80
Aly Sivji <alysivji@gmail.com>
2021-11-30 23:40:38 -0600

    completed 2021 -- day 1

---
+++ 2021/README.md
@@ -0,0 +1,27 @@
+# Advent of Code 2021
+
+[AoC 2021](https://adventofcode.com/2021)
+
+## Instructions
+
+### Run
+
+Look at `Makefile`
+
+## Test
+
+Need to figure out how to do it in TS
+
+## Daily Impressions
+
+### [Day 1: Sonar Sweep](https://adventofcode.com/2021/day/1)
+
+I spent some time after work
+setting up a TypeScript development environment
+so this wasn't as bad as doing things from scratch.
+Learning TypeScript is not like Python
+when it comes to element-wise array-maps.
+
+Got the problem done, don't think it's the nicest solution,
+but we are learning.
+What more do I want?
---
+++ 2021/day01_puzzle.ts
@@ -0,0 +1,51 @@
+"use strict";
+
+import * as fs from 'fs';
+import assert from 'assert';
+
+// part 1
+function countIncreasing(depths: Array<number>) {
+  let numIncreasing = 0;
+  let lastDepth = depths[0];
+  for (const depth of depths.slice(1)) {
+    if (depth > lastDepth) {
+      numIncreasing += 1;
+    }
+    lastDepth = depth;
+  }
+  return numIncreasing;
+}
+
+const TEST_INPUT: string = `199
+200
+208
+210
+200
+207
+240
+269
+260
+263
+`;
+const testDepths = TEST_INPUT.trim().split("\n").map(item => parseInt(item));
+console.log(countIncreasing(testDepths));
+
+const depths = fs.readFileSync('2021/data/day01_input.txt').toString().trim().split("\n").map(item => parseInt(item));
+console.log("Part 1 answer: ", countIncreasing(depths));
+
+// part 2
+function countIncreasingGroupOfThree(depths: Array<number>) {
+  let numIncreasing = 0;
+  let lastDepth: number = depths[0] + depths[1] + depths[2];
+  for (let idx = 1; idx < depths.length - 2; idx++) {
+    const depth: number = depths[idx] + depths[idx + 1] + depths[idx + 2];
+    if (depth > lastDepth) {
+      numIncreasing += 1;
+    }
+    lastDepth = depth;
+  }
+  return numIncreasing;
+}
+
+console.log(countIncreasingGroupOfThree(testDepths));
+console.log("Part 2 answer: ", countIncreasingGroupOfThree(depths));
