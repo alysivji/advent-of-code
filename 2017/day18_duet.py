9077d7f1a2e845e9af782ab90a0a8a5cf3dcf80d
Aly Sivji <alysivji@gmail.com>
2021-12-07 22:16:11 -0600

    solve 2017 -- day 18 part 2 (after 3 years)

--- 2017/day18_duet.py
+++ 2017/day18_duet.py
@@ -9,6 +9,7 @@ from typing import NamedTuple
 class NextInstruction(NamedTuple):
     continue_program: bool
     steps: int
+    waiting_to_receive: bool = False
 
 
 class SoundCard(object):
@@ -29,7 +30,7 @@ class SoundCard(object):
             elif command == 'rcv':
                 if self.values[register] != 0:
                     self.recovers = self.last_sound_played
-                    return NextInstruction(False, self.recovers)
+                    return NextInstruction(False, self.recovers, False)
         else:
             operation_with_register = instruction[2] in self.values
             if operation_with_register:
@@ -47,9 +48,9 @@ class SoundCard(object):
                 self.values[register] = self.values[register] % value
             elif command == 'jgz':
                 if self.values[register] > 0:
-                    return NextInstruction(True, value)
+                    return NextInstruction(True, value, False)
 
-        return NextInstruction(True, 1)
+        return NextInstruction(True, 1, False)
 
 
 def calculate_value_of_recovered_frequency(instructions):
@@ -59,7 +60,7 @@ def calculate_value_of_recovered_frequency(instructions):
 
     # loop thru instructions
     while True:
-        continue_program, steps = (
+        continue_program, steps, waiting_to_receive = (
             sound.process_instruction(instructions[curr_line]))
 
         if continue_program is False:
@@ -76,7 +77,10 @@ class Computer(object):
     def __init__(self, program_id, program_queue, other_queue):
         self.program_id = program_id
 
-        self.values = defaultdict(int)
+        # initialize registers
+        self.values = {}
+        for code in range(ord("a"), ord("z") + 1):
+            self.values[chr(code)] = 0
         self.values['p'] = self.program_id
 
         self.rcv_queue = program_queue
@@ -90,13 +94,15 @@ class Computer(object):
 
         if len(instruction) == 2:
             if command == 'snd':
-                self.snd_queue.append(self.values[register])
+                try:
+                    self.snd_queue.append(self.values[register])
+                except KeyError:
+                    self.snd_queue.append(int(register))
                 self.send_count += 1
             elif command == 'rcv':
-                if len(self.rcv_queue) > 0:
-                    self.values[register] = self.rcv_queue.popleft()
-                    return NextInstruction(True, 1)
-                return NextInstruction(True, 0)
+                if len(self.rcv_queue) == 0:
+                    return NextInstruction(True, 0, True)
+                self.values[register] = self.rcv_queue.popleft()
         else:
             operation_with_register = instruction[2] in self.values
             if operation_with_register:
@@ -113,16 +119,17 @@ class Computer(object):
             elif command == 'mod':
                 self.values[register] = self.values[register] % value
             elif command == 'jgz':
-                if self.values[register] > 0:
-                    return NextInstruction(True, value)
+                try:
+                    if self.values[register] > 0:
+                        return NextInstruction(True, value, False)
+                except KeyError:
+                    if int(register) > 0:
+                        return NextInstruction(True, value, False)
 
-        return NextInstruction(True, 1)
+        return NextInstruction(True, 1, False)
 
 
 def calculate_number_of_times_program1_sends_value(instructions):
-    # todo, print and walk thru via pdb or pudb
-    # use pudb to figure out what's up
-
     program0_queue = deque()
     program1_queue = deque()
 
@@ -133,16 +140,13 @@ def calculate_number_of_times_program1_sends_value(instructions):
     curr_line_program1 = 0
 
     while True:
-        continue_program0, steps_program0 = (
+        continue_program0, steps_program0, waiting_to_receive0 = (
             program0.process_instruction(instructions[curr_line_program0]))
-        continue_program1, steps_program1 = (
+        continue_program1, steps_program1, waiting_to_receive1 = (
             program1.process_instruction(instructions[curr_line_program1]))
 
-        print(continue_program0, steps_program0)
-        print(continue_program1, steps_program1)
-
         # deadlock
-        if steps_program0 == 0 and steps_program1 == 0:
+        if waiting_to_receive0 and waiting_to_receive1:
             return program1.send_count
 
         curr_line_program0 += steps_program0
@@ -176,7 +180,7 @@ if __name__ == '__main__':
     assert calculate_value_of_recovered_frequency(test_instructions) == 4
 
     instructions = {}
-    with open('day18_input.txt', 'r') as f:
+    with open('2017/data/day18_input.txt', 'r') as f:
         for count, line in enumerate(f.readlines()):
             instructions[count] = line.strip()
 
