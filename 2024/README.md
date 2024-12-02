a8d49d0e3c860e5aeb58eb0aedff9d29d1cfa30e
Aly Sivji <alysivji@gmail.com>
2024-12-02 00:02:04 -0600

    completed 2024 -- day 2

--- 2024/README.md
+++ 2024/README.md
@@ -8,6 +8,7 @@ Strengthening my TS skills by working my way through [Advent of Code 2024](https
 
 - [Daily Impressions](#daily-impressions)
   - [Day 1: Historian Hysteria](#day-1-historian-hysteria)
+  - [Day 2: Red-Nosed Reports](#day-2-red-nosed-reports)
 
 <!-- /TOC -->
 
@@ -16,3 +17,9 @@ Strengthening my TS skills by working my way through [Advent of Code 2024](https
 ### [Day 1: Historian Hysteria](https://adventofcode.com/2024/day/1)
 
 Today was easy. Since JavaScript doesn't have a `defaultdict`, it's messier than if I had used Python. :shrug: Need to see how other people solved it so I can start writing idiomatic TS.
+
+### [Day 2: Red-Nosed Reports](https://adventofcode.com/2024/day/2)
+
+Surprisingly hard puzzle... it's not really hard but I'm really rusty with my [JavaScript Array methods](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array). [Array.slice](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/slice) and [Array.splice](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/splice) FTW!
+
+Pretty happy with my solution minus the for loop. Interested to see how people do this using pure functional paradigm.
---
+++ 2024/day02_reports.ts
@@ -0,0 +1,63 @@
+import { assert } from "node:console"
+import fs from "fs";
+import { report } from "node:process";
+
+const TEST_INPUT = `7 6 4 2 1
+1 2 7 8 9
+9 7 6 2 1
+1 3 2 4 5
+8 6 4 4 1
+1 3 6 7 9
+`
+
+const parseInput = (inputString: string) => {
+  return inputString.trim().split("\n").map(report => {
+    return report.split(" ").map(Number)
+  })
+}
+
+const isReportSafe = (report: number[]) => {
+  const shifted = report.slice(1)
+  const diff = report.slice(0, -1).map((level, idx) => shifted[idx] - level)
+
+  // need to be all increasing or all decreasing
+  if (!(diff.every(value => value > 0) || diff.every(value => value < 0))) {
+    return false
+  }
+
+  // level is between 1 and 3
+  if (diff.map(level => Math.abs(level)).filter((level) => level < 1 || level > 3).length > 0) {
+    return false
+  }
+
+  return true
+}
+
+const findSafeReports = (reports: number[][]) => {
+  return reports.map(report => isReportSafe(report)).filter(value => value === true).length
+}
+
+const findSafeReportsWithProblemDampener = (reports: number[][]) => {
+  return reports.map(report => {
+    if (isReportSafe(report)) return true;
+
+    // remove one element and try it again
+    for (let i = 0; i<reports.length; i++) {
+      const reportCopy = [...report];
+      reportCopy.splice(i, 1);
+      if (isReportSafe(reportCopy)) return true;
+    }
+
+    return false;
+  }).filter(value => value === true).length
+}
+
+
+const testReports = parseInput(TEST_INPUT)
+assert(findSafeReports(testReports) === 2, "part 1 test input failed");
+assert(findSafeReportsWithProblemDampener(testReports) === 4, "part 2 test input failed")
+
+const puzzleInput = fs.readFileSync("data/day02_input.txt").toString();
+const reports = parseInput(puzzleInput);
+console.log("Part 1:", findSafeReports(reports))
+console.log("Part 2:", findSafeReportsWithProblemDampener(reports))
