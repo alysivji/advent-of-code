025bc050a8c16faf9da3b726ecc76916664bed8c
Aly Sivji <alysivji@gmail.com>
2024-12-02 10:35:29 -0600

    add prettier and format

--- 2024/day01_locations.ts
+++ 2024/day01_locations.ts
@@ -7,48 +7,58 @@ const TEST_INPUT = `3   4
 1   3
 3   9
 3   3
-`
+`;
 
 const parseInput = (input: string) => {
-  const list1: number[] = []
+  const list1: number[] = [];
   const list2: number[] = [];
-  input.trim().split("\n").forEach(item => {
-    const parts = item.split("   ");
-    list1.push(Number(parts[0]));
-    list2.push(Number(parts[1]));
-  })
-  return [list1, list2]
-}
+  input
+    .trim()
+    .split("\n")
+    .forEach((item) => {
+      const parts = item.split("   ");
+      list1.push(Number(parts[0]));
+      list2.push(Number(parts[1]));
+    });
+  return [list1, list2];
+};
 
-const distanceBetweenLocations = (list1: number[], list2: number[]) => {
-  const sortedList1 = list1.sort();
-  const sortedList2 = list2.sort();
+const distanceBetweenLocations = (left: number[], right: number[]) => {
+  const sortedLeft = left.sort();
+  const sortedRight = right.sort();
 
-  return sortedList1.map((value, idx) => Math.abs(value - sortedList2[idx])).reduce((a, b) => a + b)
-}
+  return sortedLeft
+    .map((value, idx) => Math.abs(value - sortedRight[idx]))
+    .reduce((a, b) => a + b);
+};
 
-const [a, b] = parseInput(TEST_INPUT)
-assert(distanceBetweenLocations(a, b) === 11)
+const [a, b] = parseInput(TEST_INPUT);
+assert(distanceBetweenLocations(a, b) === 11);
 
 const input = fs.readFileSync("data/day01_input.txt").toString();
-const [locationId1, locationId2] = parseInput(input);
-console.log("Part 1: ", distanceBetweenLocations(locationId1, locationId2))
+const [locationIdLeft, locationIdRight] = parseInput(input);
+console.log(
+  "Part 1:",
+  distanceBetweenLocations(locationIdLeft, locationIdRight),
+);
 
-const similartyScore = (list1: number[], list2: number[]) => {
+const similartyScore = (left: number[], right: number[]) => {
   const counts = new Map();
-  list2.forEach(item => {
+  right.forEach((item) => {
     if (counts.has(item)) {
-      counts.set(item, counts.get(item) + 1)
+      counts.set(item, counts.get(item) + 1);
     } else {
-      counts.set(item, 1)
+      counts.set(item, 1);
     }
-  })
+  });
 
-  return list1.map(item => {
-    if (!counts.has(item)) return 0
-    return item * counts.get(item)
-  }).reduce((a, b) => a + b)
-}
+  return left
+    .map((item) => {
+      if (!counts.has(item)) return 0;
+      return item * counts.get(item);
+    })
+    .reduce((a, b) => a + b);
+};
 
-assert(similartyScore(a, b) === 31)
-console.log("Part 2: ", similartyScore(locationId1, locationId2))
+assert(similartyScore(a, b) === 31);
+console.log("Part 2:", similartyScore(locationIdLeft, locationIdRight));
