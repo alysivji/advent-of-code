7301656b26bc5ff42ad4832ba21f3d8517211b53
Aly Sivji <alysivji@gmail.com>
2025-12-06 15:30:40 -0600

    2025 -- day 6 -- refactor

--- 2025/day06_math.ts
+++ 2025/day06_math.ts
@@ -12,35 +12,36 @@ type Worksheet = {
   operators: string[];
 };
 
-const parseInput = (input: string): Worksheet => {
+const solveWorksheet = (worksheet: Worksheet) => {
+  return worksheet.operators
+    .map((operator, index) => {
+      const numbers = worksheet.rows.map((row) => row[index]!);
+      const initialValue = operator === "*" ? 1 : 0;
+      return numbers.reduce(
+        (acc, value) => (operator === "*" ? acc * value : acc + value),
+        initialValue,
+      );
+    })
+    .reduce((acc, value) => acc + value);
+};
+
+// part 1
+const parseInputPart1 = (input: string): Worksheet => {
   const parsedInput = input
     .trim()
     .split(os.EOL)
     .map((line) => line.trim().split(/\s+/));
 
   return {
-    rows: parsedInput.slice(0, -1).map((row) => row.map(Number)),
-    operators: parsedInput.slice(-1)[0],
+    rows: parsedInput.slice(0, -1).map((row) => row.map(Number)!),
+    operators: parsedInput.slice(-1)[0]!,
   };
 };
 
-const part1 = (worksheet: Worksheet) => {
-  const results = worksheet.operators.map((operator, index) => {
-    const numbers = worksheet.rows.map((row) => row[index]);
-    const initialValue = operator === "*" ? 1 : 0;
-    const a = numbers.reduce(
-      (acc, value) => (operator === "*" ? acc * value : acc + value),
-      initialValue,
-    );
-    return a;
-  });
-  return results.reduce((acc, value) => acc + value);
-};
-
-// const testWorksheet = parseInput(TEST_INPUT);
+const testWorksheet = parseInputPart1(TEST_INPUT);
 
 const puzzleInput = fs.readFileSync("data/day06_input.txt").toString();
-const puzzleWorksheet = parseInput(puzzleInput);
+const worksheet = parseInputPart1(puzzleInput);
 
-// assert(part1(testWorksheet) === 4277556, "part 1 is incorrect");
-console.log("part 1:", part1(puzzleWorksheet));
+assert(solveWorksheet(testWorksheet) === 4277556, "part 1 is incorrect");
+console.log("part 1:", solveWorksheet(worksheet));
