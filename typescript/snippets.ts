82d61cf2707d81fb7d99a077d8f2bab6bd7b98f2
Aly Sivji <alysivji@gmail.com>
2025-12-09 22:51:42 -0600

    add snippet for printing grids

--- typescript/snippets.ts
+++ typescript/snippets.ts
@@ -17,4 +17,32 @@ console.log(hello());
 
 // making assertions
 import assert from "assert";
-assert(2 == 3);
+assert(false, "not matching");
+
+// printing grid
+const printGrid = (points: Point[]) => {
+  const maxX = Math.max(...points.map((point) => point.x));
+  const maxY = Math.max(...points.map((point) => point.y));
+
+  const redTiles = new GridSet();
+  points.forEach((point) => redTiles.add(point));
+
+  const lines: string[][] = [];
+  for (let y = 0; y <= maxY + 1; y++) {
+    const line: string[] = [];
+    for (let x = 0; x <= maxX + 2; x++) {
+      const isRedTile = redTiles.has(new Point(x, y));
+      if (isRedTile) {
+        line.push("#");
+      } else {
+        line.push(".");
+      }
+    }
+
+    lines.push(line);
+  }
+
+  const output = lines.map((line) => line.join("")).join("\n");
+  console.log(output);
+  // fs.writeFileSync("example.txt", output);
+};
